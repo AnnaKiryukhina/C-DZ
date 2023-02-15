4a48f662c4cbbc18f54ca405d4c04327768758a3
AnnaKiryukhina <124432443+AnnaKiryukhina@users.noreply.github.com>
2023-02-15 19:57:58 +0700

    Seminar 001 ДЗ задача 15 решение

---
+++ Seminar 02_DZ_2_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if ( number > 5 && number < 8) 
+{
+    System.Console.WriteLine(" да");
+}
+else
+{
+    System.Console.WriteLine(" нет");
+}
